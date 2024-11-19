237128066a9aafcf700139d95bea9bc4bf9bb72f
Vlad Lytvynenko <vladlyt@pm.me>
2024-11-19 10:52:38 +0100

    add minimal starship conf

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -10,3 +10,7 @@ alias less="less -R"
 alias fp="fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'"
 
 alias dp='ssh $USER.devpod-nld; tc $DEFAULT_COLOR'
+
+
+alias minimal='export STARSHIP_CONFIG="$HOME/.config/starship-minimal.toml"'
+alias normal='export STARSHIP_CONFIG="$HOME/.config/starship-$CURRENT_MACHINE.toml"'
