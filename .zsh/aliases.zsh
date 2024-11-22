7481583a2ffb36ee031a2d237ebf3a6f074b4871
Vlad Lytvynenko <vladlyt@pm.me>
2024-11-22 12:32:54 +0100

    add "ad" alias

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -11,6 +11,7 @@ alias fp="fzf --preview 'bat --style=numbers --color=always --line-range :500 {}
 
 alias dp='ssh $USER.devpod-nld; tc $DEFAULT_COLOR'
 
-
 alias minimal='export STARSHIP_CONFIG="$HOME/.config/starship-minimal.toml"'
 alias normal='export STARSHIP_CONFIG="$HOME/.config/starship-$CURRENT_MACHINE.toml"'
+
+alias ad="arc diff HEAD^"
