993dc45f7f5b5fbecd446210f6902e71c24a18a2
Vlad Lytvynenko <vladlyt@pm.me>
2025-06-17 10:57:22 +0200

    claude + bat

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -43,3 +43,21 @@ function iterm_tab_color() {
 
 alias tc='iterm_tab_color'
 
+
+claude_setup() {
+  gcloud auth application-default login;
+  gcloud auth application-default set-quota-project progsys-genai-198685;
+  export ANTHROPIC_VERTEX_PROJECT_ID="progsys-genai-198685";
+  export CLAUDE_CODE_USE_VERTEX=1;
+  export CLOUD_ML_REGION="us-east5";
+}
+
+claude() {
+  claude_setup;
+  echo "claude_setup() complete. Unsetting the function to allow original 'claude' to run directly."
+  unset -f claude
+
+  # Now, call the original claude.
+  # Since the function is unset, the shell will find the executable in PATH.
+  claude "$@"
+}
