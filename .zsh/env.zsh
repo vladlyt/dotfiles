6c32c17347f9f1e64e3c9fa2e74295a0c9803983
Vlad Lytvynenko <vladlyt@pm.me>
2025-07-12 00:48:01 +0200

    add env for claude

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -15,7 +15,10 @@ export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 export HOMEBREW_NO_ENV_HINTS=1
 
-
+# Claude setup
+export ANTHROPIC_VERTEX_PROJECT_ID="progsys-genai-198685";
+export CLAUDE_CODE_USE_VERTEX=1;
+export CLOUD_ML_REGION="us-east5";
 
 if [[ -f ~/.machine ]]; then
     export CURRENT_MACHINE=$(<~/.machine)
